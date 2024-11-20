81621f24f1afd42bb578c3b3c120d6564c3f7f7d
Jakub Wyszynski <wyszynski.jakub03@gmail.com>
2024-11-20 21:19:53 +0100

    JavaFX injection

--- projekt/src/main/java/MainLanterna.java
+++ projekt/src/main/java/MainLanterna.java
@@ -1,16 +1,10 @@
 import Controller.DictionaryController;
-import Model.DictionaryModel;
 import View.DictionaryView;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.gui.GUIScreen;
-import com.googlecode.lanterna.gui.Window;
-import com.googlecode.lanterna.gui.component.Panel;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.terminal.TerminalSize;
 
-import java.io.IOException;
-
-public class Main {
+public class MainLanterna {
     public static void main(String[] args) {
         // Inicjalizacja ekranu
         Screen screen = TerminalFacade.createScreen();
